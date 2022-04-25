208f86afa0212bc86d56ef56f58c6097a384bf9d
BRichardev <gui.resende@hotmail.com>
2022-04-25 01:18:38 -0300

    First Commit

---
+++ Duck.gd
@@ -0,0 +1,47 @@
+extends KinematicBody2D
+
+var side = 1
+var vel = Vector2()
+var speed = 100
+var fall = 1
+
+func _ready():
+	randomize()
+	$moviment.wait_time = rand_range(0.4, 2)
+	$anima.wait_time = rand_range(0.6, 1)
+
+func _process(delta):
+	# horizontal moviment
+	position.x += speed*side*delta
+	
+	# vertical moviment
+	position.y -= 140*fall*delta
+	
+	# animation mirroing
+	if side < 0:
+		$AnimatedSprite.flip_h = true
+	else:
+		$AnimatedSprite.flip_h = false
+	
+
+func _on_moviment_timeout():
+	side = side*(-1)
+
+
+func _on_anima_timeout():
+	if $AnimatedSprite.animation == "top":
+		$AnimatedSprite.animation = "side"
+	elif $AnimatedSprite.animation == "side":
+		$AnimatedSprite.animation = "top"
+		
+func mata():
+	$AnimatedSprite.animation = "scare"
+	$death.start()
+	
+
+
+
+func _on_death_timeout():
+	$AnimatedSprite.animation = "morte"
+	fall = -1
+	side = 0
